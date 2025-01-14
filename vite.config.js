584e936fa3dbbd2d78a82496cd9c8b4e700ec925
kleincai <kleincai@tencent.com>
2025-01-14 20:08:42 +0800

    🐞 fix:

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './', // 修改为相对路径
+  base: './', // 确保使用相对路径
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 }) 
